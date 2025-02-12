a42a9e18061b05beccf7f9885969d42427ce4d70
Ricardo Franco Delgado <rf041555@gmail.com>
2025-02-12 12:29:30 -0300

    feat: add config package, create dotenv variables, string conect, port and gitignore

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,7 +9,9 @@ import (
 )
 
 func main() {
+	config.Carregar()
+	fmt.Println(config.StringConexao)
 	r := router.Gerar()
 	fmt.Println("Rodando API")
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
