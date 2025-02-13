d0286ec8a3a10b3d3824e66dcf37205631215072
Ricardo Franco Delgado <rf041555@gmail.com>
2025-02-13 10:10:40 -0300

    feat: add repository package and receive db conection

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,9 +1,33 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuários"))
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorios := repositorios.NovoRepositorioDeUsuario(db)
+	repositorios.Criar(usuario)
 }
 
 func BuscandoUsuarios(w http.ResponseWriter, r *http.Request) {
